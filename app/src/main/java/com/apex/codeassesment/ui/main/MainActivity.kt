951db954452245c8c3d51c19cdccca2895da555f
Sarosh Madara <saroshmadara@gmail.com>
2023-08-21 01:12:36 +0500

    Feat MainActivity views converted to view binding

--- app/src/main/java/com/apex/codeassesment/ui/main/MainActivity.kt
+++ app/src/main/java/com/apex/codeassesment/ui/main/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.apex.codeassesment.R
 import com.apex.codeassesment.data.UserRepository
 import com.apex.codeassesment.data.model.User
+import com.apex.codeassesment.databinding.ActivityMainBinding
 import com.apex.codeassesment.di.MainComponent
 import com.apex.codeassesment.ui.details.DetailsActivity
 import javax.inject.Inject
@@ -21,14 +22,9 @@ import javax.inject.Inject
 //  Jetpack Compose.
 class MainActivity : AppCompatActivity() {
 
-  // TODO (2 points): Convert to view binding
-  private var userImageView: ImageView? = null
-  private var userNameTextView: TextView? = null
-  private var userEmailTextView: TextView? = null
-  private var seeDetailsButton: Button? = null
-  private var refreshUserButton: Button? = null
-  private var showUserListButton: Button? = null
-  private var userListView: ListView? = null
+  // TODO (2 points): Convert to view binding   (DONE)
+
+  lateinit var bi : ActivityMainBinding
 
   @Inject lateinit var userRepository: UserRepository
 
@@ -36,8 +32,8 @@ class MainActivity : AppCompatActivity() {
     set(value) {
       // TODO (1 point): Use Glide to load images after getting the data from endpoints mentioned in RemoteDataSource
       // userImageView.setImageBitmap(refreshedUser.image)
-      userNameTextView!!.text = value.name!!.first
-      userEmailTextView!!.text = value.email
+      bi.mainName!!.text = value.name!!.first
+      bi.mainEmail!!.text = value.email
       field = value
     }
 
@@ -50,23 +46,16 @@ class MainActivity : AppCompatActivity() {
 
     val arrayAdapter = ArrayAdapter<User>(this, android.R.layout.simple_list_item_1)
 
-    userImageView = findViewById(R.id.main_image)
-    userNameTextView = findViewById(R.id.main_name)
-    userEmailTextView = findViewById(R.id.main_email)
-    seeDetailsButton = findViewById(R.id.main_see_details_button)
-    refreshUserButton = findViewById(R.id.main_refresh_button)
-    showUserListButton = findViewById(R.id.main_user_list_button)
-    userListView = findViewById(R.id.main_user_list)
-    userListView!!.adapter = arrayAdapter
-    userListView?.setOnItemClickListener { parent, _, position, _ -> navigateDetails(parent.getItemAtPosition(position) as User) }
+    bi.mainUserList!!.adapter = arrayAdapter
+    bi.mainUserList?.setOnItemClickListener { parent, _, position, _ -> navigateDetails(parent.getItemAtPosition(position) as User) }
 
     randomUser = userRepository.getSavedUser()
 
-    seeDetailsButton!!.setOnClickListener { navigateDetails(randomUser) }
+    bi.mainSeeDetailsButton!!.setOnClickListener { navigateDetails(randomUser) }
 
-    refreshUserButton!!.setOnClickListener { randomUser = userRepository.getUser(true) }
+    bi.mainRefreshButton!!.setOnClickListener { randomUser = userRepository.getUser(true) }
 
-    showUserListButton!!.setOnClickListener {
+    bi.mainUserListButton!!.setOnClickListener {
       val users = userRepository.getUsers()
       arrayAdapter.clear()
       arrayAdapter.addAll(users)
